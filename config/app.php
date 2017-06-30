2962f6e75a78c1aa4e186903b5450c69450beccb
tzurbaev <zurbaev@gmail.com>
2017-06-30 17:20:02 +0800

    Setup

--- config/app.php
+++ config/app.php
@@ -167,6 +167,7 @@ return [
          * Package Service Providers...
          */
         Laravel\Tinker\TinkerServiceProvider::class,
+        Laravel\Achievements\Providers\AchievementsServiceProvider::class,
 
         /*
          * Application Service Providers...
